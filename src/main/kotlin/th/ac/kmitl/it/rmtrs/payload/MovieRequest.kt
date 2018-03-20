84ec3970b918f360b450ef29cdc86939a47ce7a1
Weeruhputt Supsohmboon <59070162@it.kmitl.ac.th>
2018-03-20 22:40:22 +0700

    Fix json attribute

--- src/main/kotlin/th/ac/kmitl/it/rmtrs/payload/MovieRequest.kt
+++ src/main/kotlin/th/ac/kmitl/it/rmtrs/payload/MovieRequest.kt
@@ -1,7 +1,9 @@
 package th.ac.kmitl.it.rmtrs.payload
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import th.ac.kmitl.it.rmtrs.definition.Rate
 import java.time.LocalDate
+import javax.validation.constraints.FutureOrPresent
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.PositiveOrZero
@@ -12,10 +14,14 @@ data class MovieRequest(
         @field:NotBlank var en_title: String = "default",
         @field:NotBlank var synopsis: String = "default",
         @field:PositiveOrZero var duration: Int = 0,
+        @JsonProperty("poster_url")
         @field:NotBlank var posterUrl: String = "default",
+        @JsonProperty("trailer_url")
         @field:NotBlank var trailerUrl: String = "default",
-        @field:NotNull var releaseDate: LocalDate = LocalDate.now(),
-        @field:NotNull var endDate: LocalDate = LocalDate.now(),
+        @JsonProperty("release_date")
+        @field:FutureOrPresent var releaseDate: LocalDate = LocalDate.now(),
+        @JsonProperty("end_date")
+        @field:FutureOrPresent var endDate: LocalDate = LocalDate.now(),
         @field:NotNull var rate: Rate = Rate.G,
         var actors: List<Long>,
         var genres: List<Long>,
