ed63a528e2363f8d2ac4f72f1f642d7825584bb4
Weeruhputt Supsohmboon <59070162@it.kmitl.ac.th>
2018-05-02 10:47:34 +0700

    Filter out is not active

--- src/main/kotlin/th/ac/kmitl/it/rmtrs/repository/AccountRepository.kt
+++ src/main/kotlin/th/ac/kmitl/it/rmtrs/repository/AccountRepository.kt
@@ -1,10 +1,13 @@
 package th.ac.kmitl.it.rmtrs.repository
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import th.ac.kmitl.it.rmtrs.model.Account
+import th.ac.kmitl.it.rmtrs.model.Theatre
 import java.util.*
 import javax.transaction.Transactional
 
@@ -18,4 +21,7 @@ interface AccountRepository: JpaRepository<Account, Long> {
     fun findByUsername(username: String): Optional<Account>
 
     fun findByUsernameAndPassword(username: String, password: String): Optional<Account>
+
+    @Query("SELECT a FROM Account a WHERE a.isActive = 1")
+    override fun findAll(pageable: Pageable): Page<Account>
 }
